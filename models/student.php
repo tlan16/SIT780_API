9c6dbd4a6da68149327f1915b669d015a242b4e2
Frank Lan <franklan118@gmail.com>
2018-05-06 21:13:32 +1000

    update and create student

--- models/student.php
+++ models/student.php
@@ -8,6 +8,26 @@ class Student
     private $id;
 
     /**
+     * @var string $email
+     */
+    private $email;
+
+    /**
+     * @var string $firstname
+     */
+    private $firstname;
+
+    /**
+     * @var string $lastname
+     */
+    private $lastname;
+
+    /**
+     * @var string $address
+     */
+    private $address;
+
+    /**
      * @var array $students
      */
     private $students = array();
@@ -15,17 +35,91 @@ class Student
     /**
      * Student constructor.
      * @param string $id
-     *
-     * @throws Exception
      */
     public function __construct($id)
     {
         $this->students = self::getAll();
-
-        if (!$this->students[$id])
-            throw new Exception("student with id `$id` does not exist.");
-
         $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname(): string
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     */
+    public function setFirstname(string $firstname): void
+    {
+        $this->firstname = $firstname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname(): string
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     */
+    public function setLastname(string $lastname): void
+    {
+        $this->lastname = $lastname;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAddress(): string
+    {
+        return $this->address;
+    }
+
+    /**
+     * @param string $address
+     */
+    public function setAddress(string $address): void
+    {
+        $this->address = $address;
     }
 
     public function delete()
@@ -33,7 +127,28 @@ class Student
         unset($this->students[$this->id]);
     }
 
+    /**
+     * Save students
+     */
     public function save()
+    {
+        if ($this->id) {
+            // init new student
+            if (!$this->students[$this->id])
+                $this->students[$this->id] = array(
+                    'student_id' => $this->id,
+                );
+
+            $this->students[$this->id]['email'] = $this->email;
+            $this->students[$this->id]['firstname'] = $this->firstname;
+            $this->students[$this->id]['lastname'] = $this->lastname;
+            $this->students[$this->id]['address'] = $this->address;
+        }
+
+        $this->writeToXml();
+    }
+
+    private function writeToXml()
     {
         $xw = xmlwriter_open_memory();
         xmlwriter_set_indent($xw, 1);
@@ -104,5 +219,11 @@ class Student
     private static function getDataFilePath()
     {
         return dirname(__FILE__) . "/../asset/students.xml";
+    }
+
+    public static function getById($id)
+    {
+        if (isset(self::getAll()[$id]))
+            return new self($id);
     }
 }
--- router.php
+++ router.php
@@ -15,23 +15,123 @@ switch (strtok($_SERVER["REQUEST_URI"], '?')) {
         }
         break;
     case '/student':
+        // delete student
         if ($_SERVER['REQUEST_METHOD'] === "DELETE") {
             if (empty($_GET['id']))
                 sendResponse(400, 'missing required query parameter `id`');
             else {
                 $id = $_GET['id'];
-
                 include_once "models/student.php";
-                try {
-                    $student = new Student($id);
-                    $student->delete();
-                    $student->save();
-                    sendResponse();
-                } catch (Exception $e) {
-                    sendResponse(400, $e->getMessage());
+
+                // check student exist
+                $student = Student::getById($id);
+                if (!$student instanceof Student) {
+                    sendResponse(400, "student with id $id does not exist");
+                    break;
                 }
+
+                $student->delete();
+                $student->save();
+                sendResponse();
                 break;
             }
+        // update student
+        } else if ($_SERVER['REQUEST_METHOD'] === "PUT") {
+            if (empty($_GET['id'])) {
+                sendResponse(400, 'missing required query parameter `id`');
+                break;
+            }
+            $id = $_GET['id'];
+            include_once "models/student.php";
+
+            // check student exist
+            $student = Student::getById($id);
+            if (!$student instanceof Student) {
+                sendResponse(400, "student with id $id does not exist");
+                break;
+            }
+
+            // get payload
+            parse_str(file_get_contents("php://input"), $payload);
+
+            // validate email
+            if (isset($payload['email'])) {
+                $email = $payload['email'];
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    sendResponse(400, 'invalid email');
+                    break;
+                }
+                else $student->setEmail($email);
+            }
+
+            // update firstname - cannot be empty
+            if (!empty($payload['firstname']))
+                $student->setFirstname($payload['firstname']);
+
+            // update lastname - cannot be empty
+            if (!empty($payload['lastname']))
+                $student->setLastname($payload['lastname']);
+
+            // update address
+            if (isset($payload['address']))
+                $student->setAddress($payload['address']);
+
+            $student->save();
+            sendResponse();
+            break;
+        // create student
+        } else if ($_SERVER['REQUEST_METHOD'] === "POST") {
+            // validate payload
+            if (empty($_POST['id'])) {
+                sendResponse(400, 'missing required query parameter `id`');
+                break;
+            } else $id = $_POST['id'];
+
+            if (empty($_POST['firstname'])) {
+                sendResponse(400, 'missing required query parameter `firstname`');
+                break;
+            } else $firstname = $_POST['firstname'];
+
+            if (empty($_POST['lastname'])) {
+                sendResponse(400, 'missing required query parameter `lastname`');
+                break;
+            } else $firstname = $_POST['lastname'];
+
+            $email = $_POST['email'] ?: '';
+            $address = $_POST['address'] ?: '';
+            include_once "models/student.php";
+
+            // validate student NOT exist
+            $student = Student::getById($id);
+            if ($student instanceof Student) {
+                sendResponse(400, "student with id $id already exist");
+                break;
+            }
+            $student = new Student($id);
+
+            // email
+            if (isset($_POST['email'])) {
+                $email = $_POST['email'];
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+                    sendResponse(400, 'invalid email');
+                else $student->setEmail($email);
+            }
+
+            // firstname - cannot be empty
+            if (!empty($_POST['firstname']))
+                $student->setFirstname($_POST['firstname']);
+
+            // lastname - cannot be empty
+            if (!empty($_POST['lastname']))
+                $student->setLastname($_POST['lastname']);
+
+            // address
+            if (isset($_POST['address']))
+                $student->setAddress($_POST['address']);
+
+            $student->save();
+            sendResponse();
+            break;
         }
         break;
 }
